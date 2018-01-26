96e4fa12d1e1f00b970a660ad12aea49af21546a
Nik Charlebois <nik_charlebois@hotmail.com>
2018-01-25 20:18:51 -0500

    Cleaned up file

--- PowerShell/Modules/PFE-SharePoint/Cmdlets/Get-SPAllUserInfo/Get-SPAllUserInfo.psm1
+++ PowerShell/Modules/PFE-SharePoint/Cmdlets/Get-SPAllUserInfo/Get-SPAllUserInfo.psm1
@@ -11,24 +11,24 @@ function Get-SPAllUserInfo
     Add-PSSnapin Microsoft.SharePoint.PowerShell -ErrorAction SilentlyContinue
 
     $site = (Get-SPWeb $Url).Site
-    $usersList = $site.RootWeb.Lists["User Information List"];
+    $usersList = $site.RootWeb.Lists["User Information List"]
 
-    $userInfo = @();
+    $userInfo = @()
 
     foreach($userItem in $usersList.Items)
     {
         $xml = $userItem.Xml
 
         $createdDate = $null        
-        $start = $xml.IndexOf("ows_Created", 0) + 13;
+        $start = $xml.IndexOf("ows_Created", 0) + 13
         if($start -ge 13)
         {
             $end = $xml.IndexOf("'", $start)
             $createdDate = $xml.Substring($start, $end-$start)
         }
 
-        $createdBy = $null        
-        $start = $xml.IndexOf("ows_Author", 0) + 12;
+        $createdBy = $null
+        $start = $xml.IndexOf("ows_Author", 0) + 12
         if($start -ge 12)
         {
             $end = $xml.IndexOf("'", $start)
