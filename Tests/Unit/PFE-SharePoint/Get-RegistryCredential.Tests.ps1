d27f777ff1416eeff68d808f5a6c17bbc931c83d
Nik Charlebois <nik_charlebois@hotmail.com>
2018-02-06 09:31:37 -0500

    Trying another approach

--- Tests/Unit/PFE-SharePoint/Get-RegistryCredential.Tests.ps1
+++ Tests/Unit/PFE-SharePoint/Get-RegistryCredential.Tests.ps1
@@ -35,7 +35,7 @@ Describe -Name $Global:TestHelper.DescribeHeader -Fixture {
         }
 
         Mock -CommandName ConvertTo-SecureString -MockWith {
-            return "fakeencryptedstring"
+            return $mockCredential.Password
         }
 
         Context -Name "When the User Info List is Found" -Fixture {
