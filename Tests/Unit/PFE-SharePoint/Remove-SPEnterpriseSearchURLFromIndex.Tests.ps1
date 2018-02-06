e874c8f0b588032c1059c7d52f7974a63d6f851e
Nik Charlebois <nik_charlebois@hotmail.com>
2018-02-06 14:21:48 -0500

    Updated codeCoverage to 90%

--- Tests/Unit/PFE-SharePoint/Remove-SPEnterpriseSearchURLFromIndex.Tests.ps1
+++ Tests/Unit/PFE-SharePoint/Remove-SPEnterpriseSearchURLFromIndex.Tests.ps1
@@ -12,38 +12,80 @@ Import-Module -Name (Join-Path -Path $PSScriptRoot `
                                 -Resolve)
 
 $Global:TestHelper = New-UnitTestHelper -SharePointStubModule $SharePointStubsModule `
-                                              -Cmdlet "Get-SPAllUserInfo"
+                                              -Cmdlet "Remove-SPEnterpriseSearchURLFromIndex"
 
 Describe -Name $Global:TestHelper.DescribeHeader -Fixture {
     InModuleScope -ModuleName $Global:TestHelper.ModuleName -ScriptBlock {
         Invoke-Command -ScriptBlock $Global:TestHelper.InitializeScript -NoNewScope
 
-        Mock -CommandName Get-SPSite -MockWith {
-            return @{
-                RootWeb = @{
-                    Lists = @{
-                        "User Information List" = @{
-                            Items = @(
-                                @{
-                                    Title = "contoso\john.smith"
-                                    Xml = "<xml ows_Created='2017-01-01' ows_author='contoso\john.smith'></xml>"
-                                }
-                            )   
+        try { [Microsoft.Office.Server.Search.Administration] }
+        catch 
+        {
+            try {
+                Add-Type -TypeDefinition @"
+                namespace Microsoft.Office.Server.Search.Administration
+                {
+                    public class CrawlLog
+                    {
+                        public CrawlLog(System.Object a)
+                        {
+
+                        }
+                        public System.Collections.Generic.List<FakeJournal> GetCrawledUrls(System.Object a, System.Object b, System.Object c, System.Object d, System.Object e, System.Object f, System.Object g, System.Object h, System.Object i)
+                        {
+                            System.Collections.Generic.List<FakeJournal> CrawledUrls = new System.Collections.Generic.List<FakeJournal>();
+                            CrawledUrls.Add(new FakeJournal());
+                            return CrawledUrls;
+                        }
+
+                        public System.Boolean RemoveDocumentFromSearchResults(System.Object a)
+                        {
+                            return true;
+                        }
+                    }
+
+                    public class FakeJournal
+                    {
+                        public System.Collections.Generic.List<FakeEntry> Rows = new System.Collections.Generic.List<FakeEntry>();
+                        public FakeJournal()
+                        {
+                            Rows.Add(new FakeEntry());
+                        }
+                    }
+
+                    public class FakeEntry
+                    {
+                        public string FullUrl;
+                        public FakeEntry()
+                        {
+                            FullUrl = "http://sharepoint.contoso.com";
                         }
                     }
                 }
-            }| Add-Member ScriptMethod Update {
-            } -PassThru
+
+"@ -ErrorAction SilentlyContinue
+            }
+            catch {
+                Write-Verbose "The type Microsoft.Office.Server.Search.Administration.CrawlLog was already added."
+            }
         }
 
-        Context -Name "When the User Info List is Found" -Fixture {
+        Mock -CommandName Get-SPEnterpriseSearchServiceApplication -MockWith {
+            return @(
+                @{
+                    Name = "Search Service Application"
+                }
+            )
+        }
+
+        Context -Name "When the item is found and properly removed" -Fixture {
             
             $testParams = @{
                 Url = "http://sharepoint.contoso.com"
             }
 
-            It "Should return contoso\john.smith" {
-                (Get-SPAllUserInfo @testParams).UserName | Should Be "contoso\john.smith"
+            It "Should not throw errors" {
+                Remove-SPEnterpriseSearchURLFromIndex @testParams 
             }
         }        
     }
