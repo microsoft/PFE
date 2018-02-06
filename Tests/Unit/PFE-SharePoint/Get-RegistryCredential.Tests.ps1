d47dda6e9905238049043bdef09a7100dbc7a04b
Nik Charlebois <nik_charlebois@hotmail.com>
2018-02-06 09:07:08 -0500

    Fixed encrypted string

--- Tests/Unit/PFE-SharePoint/Get-RegistryCredential.Tests.ps1
+++ Tests/Unit/PFE-SharePoint/Get-RegistryCredential.Tests.ps1
@@ -30,16 +30,7 @@ Describe -Name $Global:TestHelper.DescribeHeader -Fixture {
         Mock -CommandName Get-ItemProperty -MockWith {
             return @{
                 UserName = "contoso\john.smith"
-                Password = "01000000d08c9ddf0115d1118c7a00c04fc297eb01000000f`
-                            74bf2f14b6cdf4586266f16f1dfac13000000000200000000`
-                            0010660000000100002000000074cd73f55cadf2725bcb6443`
-                            1d9edef3035d12505cdbf5d93501a128e1c06385000000000e`
-                            8000000002000020000000c91bd5204db65776114afa3441a6`
-                            d91b8bb10e109357a36a08ceb4c2ae7d795310000000c68944`
-                            4aecd318866642e4d767f3b37f4000000024d0de35fd828a0b`
-                            9e0a8ce4d4717a6bc58103287ea0b19b668cb04de85ba3a7e4`
-                            12fa038452bea13b994db890a7511befbf4b7b738f91822dab`
-                            8aa3869a4bed"
+                Password = "01000000d08c9ddf0115d1118c7a00c04fc297eb01000000f74bf2f14b6cdf4586266f16f1dfac130000000002000000000010660000000100002000000074cd73f55cadf2725bcb64431d9edef3035d12505cdbf5d93501a128e1c06385000000000e8000000002000020000000c91bd5204db65776114afa3441a6d91b8bb10e109357a36a08ceb4c2ae7d795310000000c689444aecd318866642e4d767f3b37f4000000024d0de35fd828a0b9e0a8ce4d4717a6bc58103287ea0b19b668cb04de85ba3a7e412fa038452bea13b994db890a7511befbf4b7b738f91822dab8aa3869a4bed"
             }
         }
 
